4786ebfcae41ddddb6bbf3f11b3b59b15e3a8a50
Christopher Teh Boon Sung <29899182+cbsteh@users.noreply.github.com>
2020-08-02 13:20:52 +0800

    Update monospline.py
    Tidy up the code.

--- monospline.py
+++ monospline.py
@@ -1,8 +1,11 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from scipy.interpolate import CubicSpline
+# Hyman, J. M. (1983). Accurate monotonicity preserving cubic interpolation.
+#     SIAM Journal on Scientific and Statistical Computing, 4(4), 645–654.
+# original code from: https://github.com/antdvid/MonotonicCubicInterpolation
 
-class monospline:
+import numpy as np
+
+
+class MonoSpline:
     def __init__(self, x, y):
         self.x = np.array(x)
         self.y = np.array(y)
@@ -10,38 +13,50 @@ class monospline:
         self.h = self.x[1:] - self.x[:-1]
         self.m = (self.y[1:] - self.y[:-1]) / self.h
         self.a = self.y[:]
-        self.b = self.compute_b(self.x, self.y)
+        self.b = self.compute_b()
         self.c = (3 * self.m - self.b[1:] - 2 * self.b[:-1]) / self.h
         self.d = (self.b[1:] + self.b[:-1] - 2 * self.m) / (self.h * self.h)
 
-    def compute_b(self, t, r):
+    def compute_b(self):
         b = np.empty(self.n)
         for i in range(1, self.n - 1):
             is_mono = self.m[i - 1] * self.m[i] > 0
             if is_mono:
-                b[i] = 3 * self.m[i - 1] * self.m[i] / (max(self.m[i - 1], self.m[i]) + 2 * min(self.m[i - 1], self.m[i]))
+                n = max(self.m[i - 1], self.m[i]) + 2 * min(self.m[i - 1], self.m[i])
+                b[i] = 3 * self.m[i - 1] * self.m[i] / n
             else:
                 b[i] = 0
+
             if is_mono and self.m[i] > 0:
                 b[i] = min(max(0, b[i]), 3 * min(self.m[i - 1], self.m[i]))
             elif is_mono and self.m[i] < 0:
                 b[i] = max(min(0, b[i]), 3 * max(self.m[i - 1], self.m[i]))
 
-        b[0] = ((2 * self.h[0] + self.h[1]) * self.m[0] - self.h[0] * self.m[1]) / (self.h[0] + self.h[1])
-        b[self.n - 1] = ((2 * self.h[self.n - 2] + self.h[self.n - 3]) * self.m[self.n - 2]
-                         - self.h[self.n - 2] * self.m[self.n - 3]) / (self.h[self.n - 2] + self.h[self.n - 3])
+        n1 = self.h[0] + self.h[1]
+        b[0] = ((2 * self.h[0] + self.h[1]) * self.m[0] - self.h[0] * self.m[1]) / n1
+        n1 = (2 * self.h[self.n - 2] + self.h[self.n - 3]) * self.m[self.n - 2]
+        n2 = self.h[self.n - 2] * self.m[self.n - 3]
+        n3 = self.h[self.n - 2] + self.h[self.n - 3]
+        b[self.n - 1] = (n1 - n2) / n3
         return b
+
+    def _find_i(self, tau):
+        i = np.where(tau >= self.x)[0]
+        if i.size == 0:
+            i = 0
+        else:
+            i = i[-1]
+        i = min(i, self.n - 2)
+        return i
 
     def evaluate(self, t_intrp):
         ans = []
         for tau in t_intrp:
-            i = np.where(tau >= self.x)[0]
-            if i.size == 0:
-                i = 0
-            else:
-                i = i[-1]
-            i = min(i, self.n-2)
-            res = self.a[i] + self.b[i] * (tau - self.x[i]) + self.c[i] * np.power(tau - self.x[i], 2.0) + self.d[i] * np.power(tau - self.x[i], 3.0) #original curve r(t)
+            i = self._find_i(tau)
+            t1 = tau - self.x[i]
+            n2 = self.c[i] * np.power(t1, 2.0)
+            n3 = self.d[i] * np.power(t1, 3.0)
+            res = self.a[i] + self.b[i] * t1 + n2 + n3  # original curve r(t)
             ans.append(res)
         return ans
 
@@ -50,13 +65,10 @@ class monospline:
         if not hasattr(t_intrp, "__len__"):
             t_intrp = np.array([t_intrp])
         for tau in t_intrp:
-            i = np.where(tau >= self.x)[0]
-            if i.size == 0:
-                i = 0
-            else:
-                i = i[-1]
-            i = min(i, self.n-2)
-            res = self.b[i] + 2 * self.c[i] * (tau - self.x[i]) + 3 * self.d[i] * np.power(tau - self.x[i], 2.0)
+            i = self._find_i(tau)
+            t1 = tau - self.x[i]
+            n2 = self.c[i] * np.power(t1, 2.0)
+            res = self.b[i] + 2 * self.c[i] * t1 + 3 * self.d[i] * n2
             ans.append(res)
         if len(ans) == 1:
             return ans[0]
@@ -66,30 +78,13 @@ class monospline:
     def evaluate_forward(self, t_intrp):
         ans = []
         for tau in t_intrp:
-            i = np.where(tau >= self.x)[0]
-            if i.size == 0:
-                i = 0
-            else:
-                i = i[-1]
-            i = min(i, self.n-2)
-            res = self.a[i] + self.b[i] * (2 * tau - self.x[i]) + self.c[i] * (tau - self.x[i]) * (3*tau - self.x[i]) \
-                  + self.d[i] * np.power(tau - self.x[i], 2.0) * (4 * tau - self.x[i]) # d(xy)/dx
+            i = self._find_i(tau)
+            t1 = tau - self.x[i]
+            t2 = 2 * tau - self.x[i]
+            t3 = 3 * tau - self.x[i]
+            t4 = 4 * tau - self.x[i]
+            n2 = np.power(t1, 2.0)
+            # d(xy)/dx:
+            res = self.a[i] + self.b[i] * t2 + self.c[i] * t1 * t3 + self.d[i] * n2 * t4
             ans.append(res)
         return ans
-
-
-if __name__ == "__main__":
-    data = np.loadtxt('local_extrema.txt')
-    x = data[:, 0]
-    y = data[:, 1]
-    intrp = monospline(x, y)
-    x_intrp = np.linspace(min(x), max(x), 200)
-    y_intrp = intrp.evaluate(x_intrp)
-    plt.plot(x, y, 'o-', label='original points')
-    plt.plot(x_intrp, y_intrp, 'g-', label='monotone spline')
-
-    # try cubic spline
-    intrp = CubicSpline(x, y)
-    plt.plot(x_intrp, intrp(x_intrp), 'r-', label= 'cubic spline')
-    plt.legend(loc='lower right')
-    plt.show()
